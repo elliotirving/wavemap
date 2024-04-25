1ffcef752b8497794bf7b047528116bb723d4ff4
elliotirving <elliotirving44@hotmail.com>
2024-04-25 11:20:41 +0200

    Wavemap modifications, added test_pipeline launch and config

--- ros/wavemap_ros_conversions/include/wavemap_ros_conversions/geometry_msg_conversions.h
+++ ros/wavemap_ros_conversions/include/wavemap_ros_conversions/geometry_msg_conversions.h
@@ -1,19 +1,24 @@
 #ifndef WAVEMAP_ROS_CONVERSIONS_GEOMETRY_MSG_CONVERSIONS_H_
 #define WAVEMAP_ROS_CONVERSIONS_GEOMETRY_MSG_CONVERSIONS_H_
 
-#include <eigen_conversions/eigen_msg.h>
+// Error in eigen_conversions library
+// #include <eigen_conversions/eigen_msg.h> 
+
+// Manually include geometry_msgs types and convert
+#include <geometry_msgs/Vector3.h>
+#include <geometry_msgs/Quaternion.h>
 
 namespace wavemap::convert {
 inline void rosMsgToPoint3D(const geometry_msgs::Vector3& msg, Point3D& point) {
   Eigen::Vector3d point_double;
-  tf::vectorMsgToEigen(msg, point_double);
+  point_double << msg.x, msg.y, msg.z; // manual convert without eigen_conversions
   point = point_double.cast<FloatingPoint>();
 }
 
 inline void rosMsgToRotation3D(const geometry_msgs::Quaternion& msg,
                                Rotation3D& rotation) {
   Eigen::Quaterniond rotation_double;
-  tf::quaternionMsgToEigen(msg, rotation_double);
+  rotation_double = Eigen::Quaterniond{msg.w, msg.x, msg.y, msg.z}; // manual convert without eigen_conversions
   rotation = Rotation3D{rotation_double.cast<FloatingPoint>()};
 }
 
